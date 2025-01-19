ba9fd40fc94a72ab65f00b79d44fec28c48fdd8e
FELIPE TADEU PAIVA GASTON <gastonfelipe5@hotmail.com>
2025-01-19 05:08:19 +0000

    Previsão de preço de casas básico V1

--- machine_learning/python/Previsao_de_precos.py
+++ machine_learning/python/Previsao_de_precos.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as mp
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -34,26 +33,3 @@ print(f'mae: {mae:.2f}')
 print()
 print(f'rmse: {rmse:.2f}')
 print("<<<========================>>>")
-
-mp.figure(figsize=(8, 6))
-
-mp.scatter(y_teste, y_pred, color='dodgerblue', label='Previsões', alpha=0.7, s=100)
-
-mp.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.7)
-
-linha_referencia = np.linspace(min(y_teste), max(y_teste), 100)
-
-mp.plot(linha_referencia, linha_referencia, color='red', linestyle='--', label='Linha de Perfeição')
-
-mp.xlabel('Preços reais')
-
-mp.ylabel('Preços previstos')
-
-mp.title('Preços reais VS Preços Previstos', fontsize=16, fontweight='bold')
-
-mp.show()
-
-
-
-
-
